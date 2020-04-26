8878abdf4a0d34a495a3cbfec265fe1b2b17f594
chisj <905568425@qq.com>
2020-04-26 11:37:39 +0800

    添加项目后端代码

---
+++ src/main/java/org/study/car/dao/RoleUserMapper.java
@@ -0,0 +1,7 @@
+package org.study.car.dao;
+
+import org.study.car.model.domain.RoleUser;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface RoleUserMapper extends Mapper<RoleUser> {
+}
---
+++ src/main/java/org/study/car/model/domain/Role.java
@@ -0,0 +1,35 @@
+package org.study.car.model.domain;
+
+import lombok.Data;
+import org.study.car.base.BaseEntity;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Data
+@Table(name = "t_role")
+public class Role extends BaseEntity {
+
+    /**
+     * 角色编码
+     */
+    @Column(name = "role_code")
+    private String roleCode;
+
+    /**
+     * 角色名称
+     */
+    @Column(name = "role_name")
+    private String roleName;
+
+    /**
+     * 状态
+     */
+    private String status;
+
+    /**
+     * 备注
+     */
+    private String remark;
+
+}
